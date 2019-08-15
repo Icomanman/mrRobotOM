632b40e1495a1bdd39612508f5f5dd44923bb63a
icomanman <fred_barabs@yahoo.com>
2019-08-16 00:38:09 +0800

    added reference (as .md)

--- src/py/main.py
+++ src/py/main.py
@@ -1,4 +1,3 @@
-
 import win32com.client as RobotOM
 import os
 import sys
@@ -21,12 +20,13 @@ rob = RobotApplicationClass()
 rob_dir = "C:\\Program Files\\Autodesk\\Autodesk Robot Structural Analysis Professional 2017\\System\\Exe"
 rob_exe = rob_dir + "\\robot.exe"
 
-def apiCall(call="link"):
-	if call == "link":
-		print "Robot called to link. Please wait..."
-		robapp = RobotOM.Dispatch("Robot.Application")
 
-	elif call == "start":
-		print "Robot called to start."
-		robapp = os.startfile(rob_exe)
-	return robapp
+def apiCall(call="link"):
+    if call == "link":
+        print "Robot called to link. Please wait..."
+        robapp = RobotOM.Dispatch("Robot.Application")
+
+    elif call == "start":
+        print "Robot called to start."
+        robapp = os.startfile(rob_exe)
+    return robapp
